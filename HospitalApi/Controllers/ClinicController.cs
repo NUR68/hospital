c51aec6e036187ecb5d97f8d4ea00e33b760be71
NUR68 <gizemnur3327@gmail.com>
2024-02-28 21:21:29 +0300

    first commit

---
+++ HospitalApi/Controllers/ClinicController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataLayer.Models;
+
+namespace HospitalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClinicController : ControllerBase
+    {
+        private readonly db_hospitalContext _context;
+
+        public ClinicController()
+        {
+            _context = new db_hospitalContext ();
+        }
+
+        // GET: api/Clinic
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbClinic>>> GetTbClinics()
+        {
+          if (_context.TbClinics == null)
+          {
+              return NotFound();
+          }
+            return await _context.TbClinics.ToListAsync();
+        }
+
+        // GET: api/Clinic/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbClinic>> GetTbClinic(int id)
+        {
+          if (_context.TbClinics == null)
+          {
+              return NotFound();
+          }
+            var tbClinic = await _context.TbClinics.FindAsync(id);
+
+            if (tbClinic == null)
+            {
+                return NotFound();
+            }
+
+            return tbClinic;
+        }
+
+        // PUT: api/Clinic/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTbClinic(int id, TbClinic tbClinic)
+        {
+            if (id != tbClinic.ClinicId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tbClinic).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TbClinicExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clinic
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TbClinic>> PostTbClinic(TbClinic tbClinic)
+        {
+          if (_context.TbClinics == null)
+          {
+              return Problem("Entity set 'db_hospitalContext.TbClinics'  is null.");
+          }
+            _context.TbClinics.Add(tbClinic);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTbClinic", new { id = tbClinic.ClinicId }, tbClinic);
+        }
+
+        // DELETE: api/Clinic/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTbClinic(int id)
+        {
+            if (_context.TbClinics == null)
+            {
+                return NotFound();
+            }
+            var tbClinic = await _context.TbClinics.FindAsync(id);
+            if (tbClinic == null)
+            {
+                return NotFound();
+            }
+
+            _context.TbClinics.Remove(tbClinic);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TbClinicExists(int id)
+        {
+            return (_context.TbClinics?.Any(e => e.ClinicId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ HospitalApi/Controllers/DoctorController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DataLayer.Models;
+
+namespace HospitalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly db_hospitalContext _context;
+
+        public DoctorController(db_hospitalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Doctor
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbDoctor>>> GetTbDoctors()
+        {
+          if (_context.TbDoctors == null)
+          {
+              return NotFound();
+          }
+            return await _context.TbDoctors.ToListAsync();
+        }
+
+        // GET: api/Doctor/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbDoctor>> GetTbDoctor(int id)
+        {
+          if (_context.TbDoctors == null)
+          {
+              return NotFound();
+          }
+            var tbDoctor = await _context.TbDoctors.FindAsync(id);
+
+            if (tbDoctor == null)
+            {
+                return NotFound();
+            }
+
+            return tbDoctor;
+        }
+
+        // PUT: api/Doctor/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTbDoctor(int id, TbDoctor tbDoctor)
+        {
+            if (id != tbDoctor.DoctorsId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tbDoctor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TbDoctorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Doctor
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TbDoctor>> PostTbDoctor(TbDoctor tbDoctor)
+        {
+          if (_context.TbDoctors == null)
+          {
+              return Problem("Entity set 'db_hospitalContext.TbDoctors'  is null.");
+          }
+            _context.TbDoctors.Add(tbDoctor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTbDoctor", new { id = tbDoctor.DoctorsId }, tbDoctor);
+        }
+
+        // DELETE: api/Doctor/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTbDoctor(int id)
+        {
+            if (_context.TbDoctors == null)
+            {
+                return NotFound();
+            }
+            var tbDoctor = await _context.TbDoctors.FindAsync(id);
+            if (tbDoctor == null)
+            {
+                return NotFound();
+            }
+
+            _context.TbDoctors.Remove(tbDoctor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TbDoctorExists(int id)
+        {
+            return (_context.TbDoctors?.Any(e => e.DoctorsId == id)).GetValueOrDefault();
+        }
+    }
+}
